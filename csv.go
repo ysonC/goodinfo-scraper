cfefd934631387baea519f7b42cfe9d5f9dbab61
Wyson Cheng <wyson002@gmail.com>
2025-03-14 22:05:11 +0000

    refactor(remove): remove usless function

--- csv.go
+++ csv.go
@@ -5,21 +5,20 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"slices"
 )
 
-func checkSpace(record []string) {
-	if slices.Contains(record, "") {
-		fmt.Println("Empty value found,")
-		for i, v := range record {
-			if v == "" {
-				fmt.Printf("Empty value found at index %d\n", i)
-			}
-		}
-		return
-	}
-	fmt.Println("No empty value found")
-}
+// func checkSpace(record []string) {
+// 	if slices.Contains(record, "") {
+// 		fmt.Println("Empty value found,")
+// 		for i, v := range record {
+// 			if v == "" {
+// 				fmt.Printf("Empty value found at index %d\n", i)
+// 			}
+// 		}
+// 		return
+// 	}
+// 	fmt.Println("No empty value found")
+// }
 
 func readCSVFile(filePath string) {
 	file, err := os.Open(filePath)
@@ -44,8 +43,15 @@ func readCSVFile(filePath string) {
 }
 
 func main() {
+	fmt.Println("Reading CSV files for 1101")
 	readCSVFile("output_stock/1101/per.csv")
 	readCSVFile("output_stock/1101/stockdata.csv")
 	readCSVFile("output_stock/1101/monthlyrevenue.csv")
 	readCSVFile("output_stock/1101/cashflow.csv")
+
+	fmt.Println("Reading CSV files for 2330")
+	readCSVFile("output_stock/2330/per.csv")
+	readCSVFile("output_stock/2330/stockdata.csv")
+	readCSVFile("output_stock/2330/monthlyrevenue.csv")
+	readCSVFile("output_stock/2330/cashflow.csv")
 }
