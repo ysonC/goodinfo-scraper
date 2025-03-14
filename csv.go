48907ea9b5e8536a7a8a096dfc0d940a5d0bcb14
Wyson Cheng <wyson002@gmail.com>
2025-03-14 21:00:48 +0000

    feat(testing): read csv for combining csv

---
+++ csv.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"log"
+	"os"
+)
+
+func readCSVFile(filePath string) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		log.Fatalf("Error opening file: %v", err)
+	}
+	defer file.Close()
+
+	reader := csv.NewReader(file)
+	counterInt := 0
+
+	// Read line by line.
+	for {
+		_, err := reader.Read()
+		if err != nil {
+			break
+		}
+		counterInt++
+		// fmt.Println(record)
+	}
+	fmt.Printf("Total lines: %d\n", counterInt)
+}
+
+func main() {
+	readCSVFile("output_stock/1101/per.csv")
+	readCSVFile("output_stock/1101/stockdata.csv")
+	readCSVFile("output_stock/1101/monthlyrevenue.csv")
+	readCSVFile("output_stock/1101/cashflow.csv")
+}
